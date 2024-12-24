811cdc5f50ed9dfe2a7090f75259b198c575ed1d
신희성 <vvwv3092@gmail.com>
2024-12-24 15:59:39 +0900

    add: example project v0

---
+++ src/main/java/hello/springadvanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,26 @@
+package hello.springadvanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        // 저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalArgumentException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
