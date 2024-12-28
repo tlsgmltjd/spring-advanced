bfcaed0114578f1da7e4ceddaa0a65c2e343ecf0
신희성 <vvwv3092@gmail.com>
2024-12-28 15:29:28 +0900

    add: singleton bean field concurrency issues

---
+++ src/test/java/hello/springadvanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,47 @@
+package hello.springadvanced.trace.threadlocal;
+
+import hello.springadvanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); // 동시성 문제가 발생하지 않음
+        sleep(100); // 동시성 문제가 발생하지 않음
+        threadB.start();
+
+        sleep(2000); // main 스레드 종료 대기
+        log.info("main end");
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/test/java/hello/springadvanced/trace/threadlocal/code/FieldService.java
@@ -0,0 +1,26 @@
+package hello.springadvanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FieldService {
+
+    private String nameStore;
+
+    public String logic(String name) {
+        log.info("저장 name = {} -> name store = {}", name, nameStore);
+        nameStore = name;
+        sleep(1000);
+        log.info("조회 name store = {}", nameStore);
+        return nameStore;
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
